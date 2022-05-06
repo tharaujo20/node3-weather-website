cb0355a8cbd981f308eb8bf4f652eabaaff289a4
Thais Araujo <thaissaraujo2096@gmail.com>
2022-05-05 22:54:52 -0300

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 //Define paths for express cofig
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -122,6 +123,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is on up on port 300')
+app.listen(port, () => {
+    console.log('Server is on up on port '+port)
 })
